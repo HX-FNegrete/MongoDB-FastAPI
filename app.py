3ec2f80b6e0883884e5e924465ce7672f473d25f
Francisco Joaquín Negrete <fran.negrete@hotmail.com>
2023-01-05 22:53:15 -0300

    six commit

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from routes.user import user
 
-app = FastAPI()
+app = FastAPI(
+    title="REST API with FastAPI & MongoDB",
+    description="This is a Rest Api using FastAPI and MongoDB for Bootcamp pourpose",
+    version="0.0.1",
+)
 
 app.include_router(user)
